33a26ebcd4ca42faa147b7ef15f930aa405d7cc2
adoptanoob <adoptanoobinrails@gmail.com>
2014-08-03 04:10:02 +0200

    You can now update a project.

---
+++ spec/features/editing_projects_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+feature "Editing Projects" do 
+	before do 
+	  FactoryGirl.create(:project, name: "Sublime Text 2")
+
+			visit '/'
+			click_link "Sublime Text 2"
+			click_link "Edit Project"
+	end
+
+	scenario "Updating a project" do 
+			
+		fill_in "Name", with: "Sublime Text 2 pro"
+		click_button "Update Project"
+
+		expect(page).to have_content("Project has been updated.")
+	end
+
+	scenario "Updating a project with invalid attributes is bad" do 
+		fill_in "Name", with: ""
+		click_button "Update Project"
+
+		expect(page).to have_content("Project has not been updated.")
+	end
+end
