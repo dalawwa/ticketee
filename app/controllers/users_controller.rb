2ae368cebe5ecd49deef0dbe6d85f0b753600972
adoptanoob <adoptanoobinrails@gmail.com>
2014-08-04 11:49:55 +0200

    Refactored users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
   	@user = User.new
   end
@@ -14,15 +17,12 @@ class UsersController < ApplicationController
   end
 
   def show
-  	@user = User.find(params[:id])
   end
 
   def edit
-  	@user = User.find(params[:id])
   end
 
   def update
-  	@user = User.find(params[:id])
   	if @user.update(user_params)
   		flash[:notice] = "Profile has been updated."
   		redirect_to @user
@@ -33,6 +33,11 @@ class UsersController < ApplicationController
   end
 
 private
+  
+  def set_user
+  	@user = User.find(params[:id])
+  end
+
   def user_params
   	params.require(:user).permit(:name, :password, :password_confirmation)
   end
