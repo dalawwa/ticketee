3d5a6ebc809814c11edcd26be00c7f0f87c09a86
adoptanoob <adoptanoobinrails@gmail.com>
2014-08-04 12:30:21 +0200

    Implemented signing in.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
 
   resources :users
 
+  get '/signin', to: "sessions#new"
+  post '/signin', to: "sessions#create"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
